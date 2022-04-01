e644fc6b99948c6d464eae8504cadc9df9ff1a1a
Seraj ahmad <kseraj96@gmail.com>
2022-04-01 21:35:24 +0530

    commit

--- index.js
+++ index.js
@@ -12,14 +12,14 @@ setTimeout(getData, 2500);
 
 function getData() {
   header.innerHTML =
-    '<img src="https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2102&q=80" alt="" />';
+    '<img src="https://wallpapercave.com/wp/wp1894269.jpg" alt="" />';
   title.innerHTML = 'Lorem ipsum dolor sit amet';
   excerpt.innerHTML =
     'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore perferendis';
   profile_img.innerHTML =
-    '<img src="https://randomuser.me/api/portraits/men/45.jpg" alt="" />';
-  name.innerHTML = 'John Doe';
-  date.innerHTML = 'Oct 08, 2020';
+    '<img src="https://sportzwiki.com/wp-content/uploads/2021/03/AB-de-Villiers-1.jpg" alt="" />';
+  name.innerHTML = 'ABD';
+  date.innerHTML = 'Oct 01, 2021';
 
   animated_bgs.forEach((bg) => bg.classList.remove('animated-bg'));
   animated_bg_texts.forEach((bg) => bg.classList.remove('animated-bg-text'));
